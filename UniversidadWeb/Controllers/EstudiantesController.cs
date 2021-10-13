56b81b51eaf0d2cf9bdfd0b1f65b9f225423de05
Ana luisa <a2020114721@estudiantes.upsa.edu.bo>
2021-10-13 10:44:40 -0400

    Ana Luisa Practica

---
+++ UniversidadWeb/Controllers/EstudiantesController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using UniversidadWeb.Data;
+using UniversidadWeb.Models;
+
+namespace UniversidadWeb.Controllers
+{
+    public class EstudiantesController : Controller
+    {
+        private readonly UniversidadDbContext _context;
+
+        public EstudiantesController(UniversidadDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Estudiantes
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Estudiante.ToListAsync());
+        }
+
+        // GET: Estudiantes/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var universidad = await _context.Estudiante
+                .FirstOrDefaultAsync(m => m.EstudianteId == id);
+            if (universidad == null)
+            {
+                return NotFound();
+            }
+
+            return View(universidad);
+        }
+
+        // GET: Estudiantes/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Estudiantes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("EstudianteId,Nombre,Registro,Semestre")] Universidad universidad)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(universidad);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(universidad);
+        }
+
+        // GET: Estudiantes/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var universidad = await _context.Estudiante.FindAsync(id);
+            if (universidad == null)
+            {
+                return NotFound();
+            }
+            return View(universidad);
+        }
+
+        // POST: Estudiantes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("EstudianteId,Nombre,Registro,Semestre")] Universidad universidad)
+        {
+            if (id != universidad.EstudianteId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(universidad);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!UniversidadExists(universidad.EstudianteId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(universidad);
+        }
+
+        // GET: Estudiantes/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var universidad = await _context.Estudiante
+                .FirstOrDefaultAsync(m => m.EstudianteId == id);
+            if (universidad == null)
+            {
+                return NotFound();
+            }
+
+            return View(universidad);
+        }
+
+        // POST: Estudiantes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var universidad = await _context.Estudiante.FindAsync(id);
+            _context.Estudiante.Remove(universidad);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool UniversidadExists(int id)
+        {
+            return _context.Estudiante.Any(e => e.EstudianteId == id);
+        }
+    }
+}
---
+++ UniversidadWeb/Data/UniversidadDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UniversidadWeb.Models;
+
+namespace UniversidadWeb.Data
+{
+    public class UniversidadDbContext : DbContext
+    {
+        public DbSet<Universidad> Estudiante { get; set; }
+
+        public UniversidadDbContext(DbContextOptions<UniversidadDbContext> options) : base(options)
+        {
+
+        }
+
+    }
+}
